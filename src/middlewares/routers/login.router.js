f8c2737d4dc9d071dcf6498276130d1dae24dd17
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-02-08 20:36:22 -0300

    :sparkles: req-04

--- src/middlewares/routers/login.router.js
+++ src/middlewares/routers/login.router.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const generateToken = require('../../utils/generateToken');
 const validateEmail = require('../validateEmail');
+const validatePassword = require('../validatePassword');
 
 const router = express.Router();
 
-router.post('/login', validateEmail, (req, res) => {
+router.post('/login', validateEmail, validatePassword, (req, res) => {
     res.status(200).json({ token: generateToken() });
 });
 
--- src/middlewares/validateEmail.js
+++ src/middlewares/validateEmail.js
@@ -1,11 +1,10 @@
 module.exports = (req, res, next) => {
-    const { email } = req.body;
-    const regex = /\S+@\S+\.\S+/;
-    if (!email) {
-        return res.status(400).json({ message: 'O campo "email" é obrigatório' }); 
+    const { password } = req.body;
+    if (!password) {
+        return res.status(400).json({ message: 'O campo "password" é obrigatório' }); 
     }
-    if (!regex.test(email)) {
-        return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+    if (password.length < 6) {
+        return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
     }
 
     next();
