deed4e6bed92cf9839f2936547602f526f90faef
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-02-08 17:38:57 -0300

    :sparkles: readFs-done

--- src/middlewares/routers/talker.router.js
+++ src/middlewares/routers/talker.router.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const readFs = require('../../utils/readFs');
 
 const router = express.Router();
 
-router.get('/talker', (req, res) => {
-    res.status(200).send('Funfando');
+router.get('/talker', async (req, res) => {
+    const talker = await readFs();
+    console.log(talker);
 });
 
 module.exports = router;
