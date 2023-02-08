528cb29310cf9b0c3cda3152b3a3b28ccda97730
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-02-08 20:27:41 -0300

    :sparkles: validateEmail-middleware

--- src/middlewares/routers/login.router.js
+++ src/middlewares/routers/login.router.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const generateToken = require('../../utils/generateToken');
+const validateEmail = require('../validateEmail');
 
 const router = express.Router();
 
-router.post('/login', (req, res) => {
+router.post('/login', validateEmail, (req, res) => {
     res.status(200).json({ token: generateToken() });
 });
 
