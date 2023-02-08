7e4923b40f1e5da934679ef8d5280ed50e1dbea7
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-02-08 19:53:34 -0300

    :sparkles: req-03

--- src/middlewares/routers/login.router.js
+++ src/middlewares/routers/login.router.js
@@ -4,7 +4,7 @@ const generateToken = require('../../utils/generateToken');
 const router = express.Router();
 
 router.post('/login', (req, res) => {
-    res.status(200).send(generateToken());
+    res.status(200).json({ token: generateToken() });
 });
 
 module.exports = router;
