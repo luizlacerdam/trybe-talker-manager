d20cb2c7a5c054f27fadde10594571389a9f13ce
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-02-08 18:29:15 -0300

    :sparkles: login-router

--- src/middlewares/routers/index.js
+++ src/middlewares/routers/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 
 const router = express.Router();
 const talkerRouter = require('./talker.router');
+const loginRouter = require('./login.router');
 
 router.use(talkerRouter);
+router.use(loginRouter);
 
 module.exports = router;
