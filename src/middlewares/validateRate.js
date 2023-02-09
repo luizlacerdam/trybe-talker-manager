b9531c2e0b4be4ae56a17707aab4d2b823d3a1bd
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-02-09 20:12:56 -0300

    :sparkles: validateRate-middleware

--- src/middlewares/validateRate.js
+++ src/middlewares/validateRate.js
@@ -3,15 +3,12 @@ module.exports = (req, res, next) => {
       
     if ((rate < 1 || rate > 5)) {
       return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
-     }
-    
+    } 
     if (!rate) {
       return res.status(400).json({ message: 'O campo "rate" é obrigatório' });
-     }
-     
-    if (Number.isInteger(rate)) {
+    }
+    if (!Number.isInteger(rate)) {
     return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
    }
-    
     next();
   };
